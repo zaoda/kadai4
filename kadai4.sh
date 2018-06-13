2f026bbbb0d4095663e8d9c5a565fcab0b1f1cf7
inadayo <z18010yi@aiit.ac.jp>
2018-06-13 18:03:47 +0900

    Add comments for explanation of the script.

--- kadai4.sh
+++ kadai4.sh
@@ -4,15 +4,19 @@ echo "It's a Prime number!" > $$-prime
 echo "There is a number that can divide.
 So It's not a Prime number" > $$-divideble
 
+#Test Prime number.
 ./kadai3.sh 7 > $$-ans
 diff $$-ans $$-prime || exit 1
 
+#Test not Prime number
 ./kadai3.sh 8 > $$-ans
 diff $$-ans $$-divideble || exit 1
 
+#Test huge Prime number
 ./kadai3.sh 883639 > $$-ans
 diff $$-ans $$-prime || exit 1
 
+#Test huge not Prime number
 ./kadai3.sh 883638 > $$-ans
 diff $$-ans $$-divideble || exit 1
 
